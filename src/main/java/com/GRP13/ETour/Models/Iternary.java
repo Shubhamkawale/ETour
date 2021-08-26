575e0982778d009de4eff4fb117369565c8a0b3b
shubham <shubhamkawale48@gmail.com>
2021-08-26 13:51:04 +0530

    First Commit- DB pojo

--- src/main/java/com/GRP13/ETour/Models/Iternary.java
+++ src/main/java/com/GRP13/ETour/Models/Iternary.java
@@ -21,5 +21,48 @@ public class Iternary {
 	int day_no;
 	@Column(name="package_id")
 	int package_id;
+	
+	
+	public Iternary() {
+		
+	}
+	
+	
+	public Iternary(int iternary_id, String iternary_details, int day_no, int package_id) {
+		super();
+		this.iternary_id = iternary_id;
+		Iternary_details = iternary_details;
+		this.day_no = day_no;
+		this.package_id = package_id;
+	}
+	
+	
+	public int getIternary_id() {
+		return iternary_id;
+	}
+	public void setIternary_id(int iternary_id) {
+		this.iternary_id = iternary_id;
+	}
+	public String getIternary_details() {
+		return Iternary_details;
+	}
+	public void setIternary_details(String iternary_details) {
+		Iternary_details = iternary_details;
+	}
+	public int getDay_no() {
+		return day_no;
+	}
+	public void setDay_no(int day_no) {
+		this.day_no = day_no;
+	}
+	public int getPackage_id() {
+		return package_id;
+	}
+	public void setPackage_id(int package_id) {
+		this.package_id = package_id;
+	}
+	
+	
+	
 
 }
